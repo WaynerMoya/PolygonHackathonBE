8d18b2be19a78c3db267716d24db8a7402438e15
Wayner Moya <waynermoya@hotmail.com>
2022-08-18 21:45:26 -0500

    updated method getCauseByWallet

--- controllers/causesController.js
+++ controllers/causesController.js
@@ -5,6 +5,20 @@ const {
     uploadImage
 } = require('../global/upload-data')
 
+
+const consultTable = async (table, paramName, paramValue) => {
+
+    const Table = Moralis.Object.extend(table);
+
+    const query = new Moralis.Query(Table);
+
+    query.equalTo(paramName, paramValue);
+
+    const result = await query.first();
+
+    return result;
+}
+
 const causeController = {
 
     createCauseByWallet: async (req, res) => {
@@ -71,6 +85,7 @@ const causeController = {
         try {
 
             const { wallet } = req.params
+
             const { ethAddress } = req.body
 
             const Cause = Moralis.Object.extend("Causes");
@@ -78,18 +93,64 @@ const causeController = {
             const query = new Moralis.Query(Cause);
 
             query.equalTo("nftArtist", wallet);
+
             const result = await query.find();
-           
-            if(ethAddress !== wallet){
-                ///Aquiiiii                
+
+            if (ethAddress !== wallet) {
+
+                for (let i = 0; i < result.length; i++) {
+
+                    // TimeLock -> CauseTimeLock
+
+                    // Dao -> CauseDao
+
+                    // Market -> CauseMarket
+
+                    const { address } = result[i].attributes
+
+                    const timeLock = await consultTable('CauseTimeLock', 'timelockAddress', address)
+
+                    if (!timeLock) {
+                        result[i].attributes.timelockAddress = null
+                        result[i].attributes.daoAddress = null
+                        result[i].attributes.marketAddress = null
+                        continue;
+                    }
+
+                    result[i].attributes.timeLockAddress = timeLock.get('timelockAddress')
+
+                    const dao = await consultTable('CauseDao', 'daoAddress', address)
+
+                    if (!dao) {
+                        result[i].attributes.daoAddress = null
+                        result[i].attributes.marketAddress = null
+                        continue;
+                    }
+
+                    result[i].attributes.daoAddress = dao.get('daoAddress')
+
+                    const market = await consultTable('CauseMarket', 'marketAddress', address)
+
+                    if (!market) {
+                        result[i].attributes.marketAddress = null
+                        continue;
+                    }
+
+                    result[i].attributes.marketAddress = market.get('marketAddress')
+                    
+                }
+
+                ///Aquiiiii
 
             }
+
             if (!result) {
                 return res.status(200).json({
                     message: 'Error to get cause',
                     success: false
                 })
             }
+
             // const options = {
             //     chain: "mumbai",
             //     address: result.get('contractAddress')
@@ -123,8 +184,8 @@ const causeController = {
             query.equalTo("contractAddress", address);
 
             const result = await query.find();
-           
-              
+
+
             if (!result) {
                 return res.status(200).json({
                     message: 'Error to get cause',
